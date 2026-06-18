a7f14895766279b7b4ebbedb91d467ef65c86dc9
逸风 <jmxw0814@gmail.com>
2026-06-18 16:15:12 +0800

    Merge to main and Modify the configuration

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
-  branch: "feature/tinacms2",
+  branch: "main",
 
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   token: process.env.TINA_TOKEN!,
